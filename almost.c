8f1e70497775d5bf3ec681a95a7452d903128762
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2013-02-07 19:35:23 +0000

    add; print ast as sexp

--- almost.c
+++ almost.c
@@ -8,14 +8,21 @@ obj List
     data (* void)
 
     def append (new_element (* List)) (* List)
-        while (isnt .next NULL)
-            = . .next
-        = .next new_element
+        while (isnt (-> self next) NULL)
+            = self (-> self next)
+        = (-> self next) new_element
         return new_element
 
+typedef int Int_t
 obj Int
     def new () (* int)
         = i-pointer (cast (* int) (malloc (sizeof int))) 
         = (deref i-pointer) 0
-        return 0
+        return i-pointer 
+
+    def append (list (* List) n int)
+        = new_element (new List)
+        = new_int (new Int)
+        = (-> new_element data) new_int
+        list:append new_element
 
--- pf_parser.py
+++ pf_parser.py
@@ -6,11 +6,14 @@ pp = pprint.PrettyPrinter(indent=2).pprint
 
 def main():
     input_text = open(argv[1]).read()
-    print(input_text)
+    #print(input_text)
     ts = Tokenizer(input_text)
     statements = parse_tokens(ts)
-    print('\n')
-    pp (statements)
+    #print('\n')
+    #pp (statements)
+    for s in statements:
+        print(indent(s))
+        print()
 
 def ast(text):
     return parse_tokens(Tokenizer(text))
@@ -66,6 +69,44 @@ def parse_sexp(token_stream):
     token_stream.skip('CLOSE_PAREN')
 
     return sexp
+
+def indent(e, level=0, start=''):
+    if isinstance(e, list):
+        try:
+            if e[0] == 'def':
+                head, name, args, return_value, *tail = e
+                body = '%s %s %s %s %s' % (
+                        head,
+                        name,
+                        to_string(args),
+                        to_string(return_value),
+                        ' '.join(indent(t, level + 1, '\n') for t in tail))
+            elif e[0] == 'obj':
+                head, name, *tail = e 
+                s = []
+                for a in tail:
+                    if a[0] == 'def':
+                        s.append(indent(a, level + 1, '\n'))
+                    else:
+                        s.append('\n%s%s' % ('  ' * (level + 1), to_string(a)))
+                body = 'obj %s %s' % (name, ' '.join(s))
+            else:
+                body = ' '.join(indent(t, level + 1, '\n') for t in e)
+        except IndexError:
+            body = ''
+
+        return '%s%s(%s)' %  (
+                start,
+                '  ' * level,
+                body)
+    else:
+        return e
+
+def to_string(e):
+    if isinstance(e, list):
+        return '(%s)' % ' '.join(to_string(t) for t in e)
+    else:
+        return e
 
 if __name__ == '__main__':
     main()
