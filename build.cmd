638f1a1f2c6e2f98a379b29b69e5a7e6401bd0e8
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2013-02-14 20:08:46 +0000

    fix; build.cmd to work with other files

--- build.cmd
+++ build.cmd
@@ -1,16 +1,16 @@
 @echo off
 
 if "%1"=="" (
-    goto :default
+    call :compile almost.c
 ) else (
-    goto :%1
+    call :%1 %2
 )
-echo err: Unknown task
 exit /b
 
 
-:default
-    python compile.py almost.c
+:c
+:compile
+    python compile.py %1
     exit /b %ERRORLEVEL%
 
 :test
@@ -19,21 +19,21 @@ exit /b
 
 :g
 :gcc
-    call :default > out.c && gcc -Wall -g out.c
+    call :compile %1 > out.c && gcc -Wall -g out.c
     exit /b %ERRORLEVEL%
 
 :r
 :run
-    call :gcc && a.exe
+    call :gcc %1 && a.exe
     exit /b %ERRORLEVEL%
 
 :p
 :parse
-    python pf_parser.py almost.c
+    python pf_parser.py %1
     exit /b %ERRORLEVEL%
 
 :t
 :tokens
-    python Tokenizer.py almost.c
+    python Tokenizer.py %1
     exit /b %ERRORLEVEL%
 
