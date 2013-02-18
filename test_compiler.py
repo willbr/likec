fc138158d2acdeca5237edd5e79698433d154eb8
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2013-02-18 17:21:00 +0000

    add; tests; compile_range

--- test_compiler.py
+++ test_compiler.py
@@ -1,15 +1,33 @@
 import unittest
-from prefix_compiler import parse
+import prefix_compiler
 
 class TestPrefixCompiler(unittest.TestCase):
 
     def test_parse(self):
         self.assertEqual(
-                parse('''
+                prefix_compiler.parse('''
 def hyphen-test
     prn "hello"
                 ''')[0],
                 ['def', 'hyphen_test', [], 'void', ['prn','"hello"']]
+                )
+
+    def test_compile_range_end(self):
+        self.assertEqual(
+                prefix_compiler.compile_range('10'),
+                ['0', '10', '1']
+                )
+
+    def test_compile_range_start_end(self):
+        self.assertEqual(
+                prefix_compiler.compile_range('1', '10'),
+                ['1', '10', '1']
+                )
+
+    def test_compile_range_start_end_step(self):
+        self.assertEqual(
+                prefix_compiler.compile_range('1', '10', '2'),
+                ['1', '10', '2']
                 )
 
 if __name__ == '__main__':
