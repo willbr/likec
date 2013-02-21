7d439967344069e3f42daa3fc81d638e80e994bb
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2013-02-21 11:34:49 +0000

    add; tests Tokenizer; heredoc & anon funcs {+ $ $}

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -1,4 +1,5 @@
 import unittest
+import prefix_tokenizer
 from prefix_tokenizer import split_id
 
 class TestTokenizer_split_id(unittest.TestCase):
@@ -32,6 +33,58 @@ class TestTokenizer_split_id(unittest.TestCase):
                 split_id('a:append'),
                 ['method', 'a', 'append']
                 )
+
+    def test_Tokenizer_single_line(self):
+        t = prefix_tokenizer.Tokenizer('''
+= a 5
+''')
+        tokens = values(t.tokens)
+
+        self.assertEqual(
+                tokens,
+                ['=', 'a', '5', '\n']
+                )
+
+    def test_Tokenizer_heredoc(self):
+        t = prefix_tokenizer.Tokenizer('''
+= a <<MSG
+hello
+world
+MSG
+''')
+        tokens = values(t.tokens)
+
+        self.assertEqual(
+                tokens,
+                ['=', 'a', '"hello\\nworld"', '\n']
+                )
+
+    def test_Tokenizer_anonymous(self):
+        t = prefix_tokenizer.Tokenizer('''
+reduce {+ $ $} a
+''')
+        tokens = values(t.tokens)
+
+        self.assertEqual(
+                tokens,
+                [
+                    'reduce',
+                    '(',
+                    'fn-shorthand',
+                    '+',
+                    '$',
+                    '$',
+                    ')',
+                    'a',
+                    '\n'
+                    ]
+                )
+
+def values(tokens):
+    return [t.value for t in tokens]
+
+def types(tokens):
+    return [t.typ for t in tokens]
 
 if __name__ == '__main__':
     unittest.main()
